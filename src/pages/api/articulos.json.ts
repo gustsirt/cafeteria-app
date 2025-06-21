d0731441005a7a73f1735d462ec5f96ef1fc6512
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-06-20 21:36:28 -0300

    🐾 update page: api/articulos.json.ts

--- src/pages/api/articulos.json.ts
+++ src/pages/api/articulos.json.ts
@@ -1,9 +1,16 @@
 import type { APIRoute } from "astro";
 import { getArticulos } from "../../lib/googleSheets.ts";
 
-export const GET: APIRoute = async ({ url }) => {
-  const rango = url.searchParams.get("range") || "";
-  if (rango == "") return new Response("Rango Articulos no válido");
+export const GET: APIRoute = async ({ request }) => {
+  const { searchParams } = new URL(request.url);
+  const rango = searchParams.get("range") || "TPRODUCTOS";
+
+  if (!rango) {
+    return new Response("Rango Articulos no válido", { status: 400 });
+  }
+
   const articulos = await getArticulos(rango);
-  return new Response(JSON.stringify(articulos));
+  return new Response(JSON.stringify(articulos), {
+    headers: { "Content-Type": "application/json" },
+  });
 };

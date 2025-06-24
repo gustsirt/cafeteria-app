7d1408e944144bea3cb641f9ba16d4142c7d12f6
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-06-23 23:45:36 -0300

    ✨ update env variables - MODE

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,27 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
   env: {
     schema: {
-      GOOGLE_SERVICE_ACCOUNT_EMAIL: envField.string({ context: "server", access: "secret" }),
-      GOOGLE_PRIVATE_KEY: envField.string({ context: "server", access: "secret" }),
-      GOOGLE_SHEETS_ID: envField.string({ context: "server", access: "secret" }),
+      GOOGLE_SERVICE_ACCOUNT_EMAIL: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+      GOOGLE_PRIVATE_KEY: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+      GOOGLE_SHEETS_ID: envField.string({
+        context: "server",
+        access: "secret",
+      }),
       CONFIG_SHEET: envField.string({ context: "server", access: "secret" }),
       ARTICLES_SHEET: envField.string({ context: "server", access: "secret" }),
       ORDER_SHEET: envField.string({ context: "server", access: "secret" }),
       TOTAL_MESAS: envField.string({ context: "server", access: "secret" }),
-    }
-  }
+      MODE: envField.string({ context: "server", access: "secret" }),
+    },
+  },
 });
--- src/pages/api/tables.json.ts
+++ src/pages/api/tables.json.ts
@@ -1,83 +1,90 @@
 import type { APIRoute } from "astro";
 import { getTablesOrders } from "../../lib/googleSheets";
+import { MODE } from "astro:env/server";
+
+const mocktables = [
+  {
+    mesa: "1",
+    productos: [
+      {
+        id: "2",
+        codigo: "1030",
+        cantidad: 2,
+        mozo: "Gustavo",
+        precio: 1,
+        fecha: "22/6/2025 10:55",
+      },
+    ],
+  },
+  {
+    mesa: "2",
+    productos: [
+      {
+        id: "1",
+        codigo: "1006",
+        cantidad: 1,
+        mozo: "Gustavo",
+        precio: 1,
+        fecha: "22/6/2025 10:55",
+      },
+      {
+        id: "3",
+        codigo: "1030",
+        cantidad: 2,
+        mozo: "Gustavo",
+        precio: 1,
+        fecha: "22/6/2025 10:55",
+      },
+    ],
+  },
+  {
+    mesa: "3",
+    productos: [],
+  },
+  {
+    mesa: "4",
+    productos: [],
+  },
+  {
+    mesa: "5",
+    productos: [],
+  },
+  {
+    mesa: "6",
+    productos: [],
+  },
+  {
+    mesa: "7",
+    productos: [],
+  },
+  {
+    mesa: "8",
+    productos: [],
+  },
+  {
+    mesa: "9",
+    productos: [],
+  },
+  {
+    mesa: "10",
+    productos: [],
+  },
+  {
+    mesa: "11",
+    productos: [],
+  },
+  {
+    mesa: "12",
+    productos: [],
+  },
+];
 
 export const GET: APIRoute = async () => {
-  // const tables = await getTablesOrders();
-  const tables = [
-    {
-      mesa: "1",
-      productos: [
-        {
-          id: "2",
-          codigo: "1030",
-          cantidad: 2,
-          mozo: "Gustavo",
-          precio: 1,
-          fecha: "22/6/2025 10:55",
-        },
-      ],
-    },
-    {
-      mesa: "2",
-      productos: [
-        {
-          id: "1",
-          codigo: "1006",
-          cantidad: 1,
-          mozo: "Gustavo",
-          precio: 1,
-          fecha: "22/6/2025 10:55",
-        },
-        {
-          id: "3",
-          codigo: "1030",
-          cantidad: 2,
-          mozo: "Gustavo",
-          precio: 1,
-          fecha: "22/6/2025 10:55",
-        },
-      ],
-    },
-    {
-      mesa: "3",
-      productos: [],
-    },
-    {
-      mesa: "4",
-      productos: [],
-    },
-    {
-      mesa: "5",
-      productos: [],
-    },
-    {
-      mesa: "6",
-      productos: [],
-    },
-    {
-      mesa: "7",
-      productos: [],
-    },
-    {
-      mesa: "8",
-      productos: [],
-    },
-    {
-      mesa: "9",
-      productos: [],
-    },
-    {
-      mesa: "10",
-      productos: [],
-    },
-    {
-      mesa: "11",
-      productos: [],
-    },
-    {
-      mesa: "12",
-      productos: [],
-    },
-  ];
+  let tables = [];
+  if (MODE != "DEVELOP") {
+    tables = await getTablesOrders();
+  } else {
+    tables = mocktables;
+  }
   return new Response(JSON.stringify(tables));
 };
